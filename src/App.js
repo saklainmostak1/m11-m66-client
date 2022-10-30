2c1db04b0a044d444013d9a2fbaf29db2dd9766a
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-31 03:22:25 +0600

    push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './Router/Routes/Routes';
 
 function App() {
+ 
   return (
     <div >
-      <button className="btn btn-warning">Warning</button>
-      <button className="btn btn-error">Error</button>
+      <RouterProvider router={router} ></RouterProvider>
     </div>
   );
 }
