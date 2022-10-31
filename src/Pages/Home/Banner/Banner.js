ae45bc79493903e9e9e72d742e18799d9372c138
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-31 06:01:01 +0600

    push

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Banner.css'
+
 import img1 from '../../../assets/images/banner/1.jpg'
 import img2 from '../../../assets/images/banner/2.jpg'
 import img3 from '../../../assets/images/banner/3.jpg'
@@ -48,7 +48,7 @@ const bannerData = [
 ]
 const Banner = () => {
     return (
-            <div className="carousel w-full">
+            <div className="carousel w-full py-10">
 
                 {
                     bannerData.map(slide => <BannerItem
@@ -56,14 +56,7 @@ const Banner = () => {
                     slide={slide}
                     ></BannerItem>)
                 }
-                {/* <BannerItem image={img1}></BannerItem> */}
-                {/* <div id="slide2" className="carousel-item relative w-full">
-                    <img src={img2} alt='' className="w-full" />
-                    <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-                        <a href="#slide1" className="btn btn-circle mr-5">❮</a>
-                        <a href="#slide3" className="btn btn-circle">❯</a>
-                    </div>
-                </div> */}
+                
               </div>  
     );
 };
