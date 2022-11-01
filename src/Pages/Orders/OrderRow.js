b45cde4603fa04000999c3492d0dd95d4794f8fa
saklainMostake <saklain15-12003@diu.edu.bd>
2022-11-02 03:15:12 +0600

    push

---
+++ src/Pages/Orders/OrderRow.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const OrderRow = () => {
+    return (
+        <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
+        <td>
+          <div className="flex items-center space-x-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src="/tailwind-css-component-profile-5@56w.png" alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">Yancy Tear</div>
+              <div className="text-sm opacity-50">Brazil</div>
+            </div>
+          </div>
+        </td>
+        <td>
+          Wyman-Ledner
+          <br/>
+          <span className="badge badge-ghost badge-sm">Community Outreach Specialist</span>
+        </td>
+        <td>Indigo</td>
+        <th>
+          <button className="btn btn-ghost btn-xs">details</button>
+        </th>
+      </tr>
+    );
+};
+
+export default OrderRow;
--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../context/AuthProvider/AuthProvider';
 
 const Orders = () => {
+    const {user} = useContext(AuthContext)
+    const [orders, setOrders] = useState({})
+
+    useEffect(() => {
+        fetch( `http://localhost:5000/orders?email=${user?.email}`)
+        .then(Response => Response.json())
+        .then(data => setOrders(data))
+    },[user?.email])
     return (
         <div>
-            <h2>This is order</h2>
+            <h2 className="text-5xl">You Have {orders.length}</h2>
+            <div className="overflow-x-auto w-full">
+  <table className="table w-full">
+   
+    <thead>
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody> 
+      
+    </tbody>
+
+    <tfoot>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+        <th></th>
+      </tr>
+    </tfoot>
+    
+  </table>
+</div>
         </div>
     );
 };
